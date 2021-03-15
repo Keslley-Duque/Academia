36b2e5d6c276e419002dab60e24144941b68c963
Keslley Duque <78504780+Keslley-Duque@users.noreply.github.com>
2021-03-15 15:08:22 -0300

    Add files via upload

--- src/academia/Aluno.java
+++ src/academia/Aluno.java
@@ -1,5 +1,57 @@
 package academia;
 
 public class Aluno {
-    
-}
+    // atributos
+    private String nome;
+    private float peso;
+    private float altura;
+
+    // metodos de acesso (set e get)
+    public void setNome(String nomeAluno) {
+        nome = nomeAluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float imc() {
+        return peso / (altura * altura);
+    }
+
+    public String situacao() {
+        float imc = imc();
+
+        if (imc < 18.6) {
+            return "Abaixo do peso!";
+        } else if (imc <= 24.9) {
+            return "Peso normal";
+        } else if (imc <= 30) {
+            return "Excesso de peso!";
+        } else {
+            return "Obesidade!";
+        }
+    }
+
+    public String resultado() {
+        return "Ficha avaliativa de " + nome + "\nPeso: " + peso + "\nAltura: " + altura + "\nIMC: " + imc()
+                + "\nSituação: " + situacao();
+    }
+
+}// fim da classe
--- src/academia/Principal.java
+++ src/academia/Principal.java
@@ -1,5 +1,29 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+package academia;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+    public static void main(String[] args) {
+        Aluno objAluno = new Aluno(); // instanciacao (criação) do objeto
+        // solicitacão das informações do aplicativo
+        String nome = JOptionPane.showInputDialog(null, "Informe o nome do aluno: ", "Academia 1.0", 3);
+
+        float altura = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite a altura de " + nome, "Academia 1.0",
+                JOptionPane.QUESTION_MESSAGE));
+
+        float peso = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite o peso de " + nome, "Academia 1.0",
+                JOptionPane.QUESTION_MESSAGE));
+
+        // adiconar os metodos de acesso (set) para setar as informações aos atributos
+        objAluno.setNome(nome);
+        objAluno.setAltura(altura);
+        objAluno.setPeso(peso);
+
+        // exibir o resultado para o usuario
+        JOptionPane.showMessageDialog(null, objAluno.resultado(), "Academia 1.0", 1);
+
+        System.exit(0);
+
     }
 }
